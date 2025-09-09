1c38ebf5fa924fc1c01fe84adfde5c92548a48a9
rhdelife <jhkong03@naver.com>
2025-09-10 00:31:34 +0900

    feat: mini-portfolio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<!-- <link rel="icon" href={favicon} /> -->
+	 <title>AJOU Mini Porfolio</title>
+	 <meta name="description" content="SvelteKit + Vercel mini portfolio" />
 </svelte:head>
 
-{@render children?.()}
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/projects">Projects</a>
+</nav>
+<main>
+	{@render children()}
+</main>
+
+<style global>
+	@import './app.css';
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1> -->
+<!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<script>
+  let name = $state('');
+  let welcome = $derived(name ? 'Hello,${name}!' : 'Welcome!');
+
+  function randomize() {
+    const msgs =['웹 개발 재밌다!', 'SvelteKit 금방 익힘', 'Vercel 배포도 쉽다!'];
+    alert(msgs[Math.floor(Math.random() * msgs.length)]);
+
+  }
+</script>
+
+<h1>{welcome}</h1>
+
+<label>
+  Your name: <input placeholder="type your name" bind:value={name} />
+</label>
+<p>미리보기: <strong>{name || '(입력대기)' }</strong></p>
+
+<button onclick={randomize}>랜덤 메세지</button>
