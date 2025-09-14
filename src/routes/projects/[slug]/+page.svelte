e6c2ddd692c13a31990b4ceee4da57931b2620d9
rhdelife <jhkong03@naver.com>
2025-09-14 19:25:52 +0900

    feat: mini-portfolio

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -23,6 +23,4 @@ function save() {
 <textarea rows = "6" bind:value={memo} class="card"  style="width:100%"></textarea>
 <button onclick ={save}> 메모 저장 </button>
 <p style="opacity:.6">브라우저 로컬에 저장됩니다 </p>
-{:else if data.slug === 'timetable'}
-<p>시간표는 아직 준비중입니다 ㅠㅠ</p>
-{/if} 
+{/if}
